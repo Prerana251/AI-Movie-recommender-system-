16de43053eb4a3de905d0fcba17db1fb4d8cfc0e
Prerana251 <rvim25pgd008.rvim@rvei.edu.in>
2026-07-29 21:52:33 +0530

    app.py

--- app.py
+++ app.py
@@ -599,9 +599,9 @@ movie is recommended.
             "Precision@10 & Recall@10"
         ],
         "Result": [
-            "Your Precision",
-            "Your RMSE / MAE",
-            "Your Precision & Recall"
+            "1.0",
+            "PMSE: 0.88 | MAE:0.63",
+            "Precision:1.0 | Recall:0.0004"
         ]
     })
 
